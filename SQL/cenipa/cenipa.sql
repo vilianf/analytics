cccb44204e082f56fa0fe1df81916feffbbfa2da
vilianf <77406362+vilianf@users.noreply.github.com>
2022-08-10 16:44:07 -0300

    Update cenipa.sql

--- SQL/cenipa/cenipa.sql
+++ SQL/cenipa/cenipa.sql
@@ -1,6 +1,6 @@
 #Acidentes de helicoptero com fatalidades
 SELECT codigo_ocorrencia, 
-	   ocorrencia_dia, 
+	ocorrencia_dia, 
        aeronave_tipo_veiculo, 
        aeronave_fatalidades_total
 FROM cenipa.aeronave A, cenipa.ocorrencia O
@@ -10,7 +10,7 @@ AND A.aeronave_fatalidades_total > 0;
 
 #Acidentes com maior numero de fatalidades
 SELECT A.ocorrencia_dia,
-	   B.aeronave_tipo_veiculo,
+	B.aeronave_tipo_veiculo,
        B.aeronave_fatalidades_total
 FROM cenipa.ocorrencia A, cenipa.aeronave B
 WHERE A.codigo_ocorrencia = B.codigo_ocorrencia2
@@ -20,7 +20,7 @@ LIMIT 10;
 
 #Fatores mais presentes em acidentes com fatalidades
 SELECT fator_nome,
-	   COUNT(*) AS Quantidade
+	COUNT(*) AS Quantidade
 FROM cenipa.fator_contribuinte A, cenipa.ocorrencia B, cenipa.aeronave C
 WHERE A.codigo_ocorrencia3 = B.codigo_ocorrencia
 AND B.codigo_ocorrencia = C.codigo_ocorrencia2
@@ -30,15 +30,15 @@ ORDER BY 2 DESC;
 
 #Dias em que ocorreram fatalidades
 SELECT DATE(STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y')) AS Data,
-	   SUM(B.aeronave_fatalidades_total) OVER (PARTITION BY DATE(STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y'))) AS Quantidade_fatalidades
+	SUM(B.aeronave_fatalidades_total) OVER (PARTITION BY DATE(STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y'))) AS Quantidade_fatalidades
 FROM cenipa.ocorrencia A, cenipa.aeronave B
 WHERE A.codigo_ocorrencia = B.codigo_ocorrencia2
 AND B.aeronave_fatalidades_total > 0;
 
 #Tempo medio entre data do acidente ate o relatorio da cenipa
 SELECT A.codigo_ocorrencia,
-	   DATE(STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y')) AS Data_acidente,
-	   B.recomendacao_dia_assinatura AS Data_Relatorio,
+	DATE(STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y')) AS Data_acidente,
+	B.recomendacao_dia_assinatura AS Data_Relatorio,
        ROUND(AVG(DATEDIFF(B.recomendacao_dia_assinatura, STR_TO_DATE(A.ocorrencia_dia, '%d/%m/%Y'))), 0) AS Tempo_Media_Dias
 FROM cenipa.ocorrencia A, cenipa.recomendacao B
 WHERE A.codigo_ocorrencia = B.codigo_ocorrencia4
@@ -46,14 +46,14 @@ GROUP BY 1;
 
 #Quais modelos com mais ocorrencias?
 SELECT aeronave_modelo,
-	   COUNT(DISTINCT codigo_ocorrencia2) AS Quantidade
+	COUNT(DISTINCT codigo_ocorrencia2) AS Quantidade
 FROM cenipa.aeronave
 GROUP BY 1
 ORDER BY 2 DESC;
 
 #Em ocorrencias com fatalidades, quais os principais operadores?
 SELECT aeronave_operador_categoria,
-	   COUNT(DISTINCT codigo_ocorrencia2) AS Quantidade
+	COUNT(DISTINCT codigo_ocorrencia2) AS Quantidade
 FROM cenipa.aeronave
 WHERE aeronave_fatalidades_total > 0
 GROUP BY 1
