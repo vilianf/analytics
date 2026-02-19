c7aab64aa8d68669b78264a3fa0bf2392c348efd
vilianf <77406362+vilianf@users.noreply.github.com>
2026-02-19 17:11:09 -0300

    Update queries_fire_incidents.sql

--- SQL/data_san_francisco/queries_fire_incidents.sql
+++ SQL/data_san_francisco/queries_fire_incidents.sql
@@ -1,36 +1,36 @@
-#Acumulado de ocorrencias durante o ano de 2021 por dia
+# Acumulado de ocorrencias durante o ano de 2021 por dia
 SELECT DISTINCT `Incident Date`,
 	   COUNT(ID) OVER (PARTITION BY DATE(CAST(`Incident Date` AS DATE)) ORDER BY `Incident Date`) as Quantidade
 FROM datasf.fire_incidents;
 
-#Meses com maior numero de incidentes com fogo
+# Meses com maior numero de incidentes com fogo
 SELECT MONTH(`Incident Date`) as Mes, YEAR(`Incident Date`) AS Ano, COUNT(ID) AS Quantidade_Mes
 FROM datasf.fire_incidents
 GROUP BY 1, 2
 ORDER BY 3 DESC;
 
 
-#Feridos
+# Feridos
 SELECT ID, `Incident Date`, `Fire Injuries`, `Civilian Injuries`
 FROM datasf.fire_incidents
 WHERE `Civilian Fatalities` > 0
 OR `Fire Injuries` > 0
 ORDER BY 3, 4 DESC;
 
-#Mortes
+# Mortes
 SELECT ID, `Incident Date`, `Fire Fatalities`, `Civilian Fatalities`
 FROM datasf.fire_incidents
 WHERE `Civilian Fatalities` > 0
 OR `Fire Fatalities` > 0
 ORDER BY 3, 4 DESC;
 
-#Numero de ocorrencias por bairro
+# Numero de ocorrencias por bairro
 SELECT neighborhood_district, COUNT(id) as quantidade_bairro
 FROM datasf.fire_incidents
 GROUP BY 1
 ORDER BY 2 DESC;
 
-#Tempo entre acionamento até chegada ao local do incidente
+# Tempo entre acionamento até chegada ao local do incidente
 SELECT resultado.id, 
        resultado.Hora_acionamento, 
        resultado.Hora_Chegada,
